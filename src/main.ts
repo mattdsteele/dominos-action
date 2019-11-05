35b1d80742b7f68564a556e73de54ba0e98b1ab7
mattdsteele <orphum@gmail.com>
2019-11-04 18:38:03 -0600

    Add Issue creation step

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import * as core from '@actions/core';
-import { wait } from './wait';
+import { GitHub, context } from '@actions/github';
 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`);
-
-    core.debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    core.debug(new Date().toTimeString());
+    const token = core.getInput('github-token', { required: true });
+    const github = new GitHub(token);
+    const issue = await github.issues.create({
+      title: '🍕 time',
+      body: `# Time to put some content in here.`,
+      ...context.repo
+    });
+    console.log(issue.status);
+    console.log(issue.data.id);
+    console.log(JSON.stringify(issue.data));
 
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
